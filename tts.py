ae6c2ebd9376abc98db5ba14ebf5c3f83a1ef035
Rokossuwakowski <cotacminnus@gmail.com>
2024-10-08 21:55:57 -0500

    pyttsx3 library is included

--- tts.py
+++ tts.py
@@ -1,8 +1,27 @@
+import pyttsx3
 
-def text_to_speech(text, online):
+# This is implemented as a class for convenience
+class Text2Speech:
+    engine = None
+    volume = 100    # in case we need it
 
-    return speech   # returns an audio file
+    def init(self):
+        self.engine = pyttsx3.init()
 
-def connect(token):
-                    # use API if necessary
-    return False
+    def text_to_speech(self, text):
+        self.engine.say(text)
+
+        self.engine.runAndWait()
+        # pyttsx3 will output the audio to the speaker, no return needed
+        # This will probably be blocking
+
+        # return speech   # returns an audio file if we decide to implement otherway later
+
+    '''
+
+    ### If we decide to go online then we will have to implement this
+
+    def connect(token):
+                        # use API if necessary
+        return False
+    '''
