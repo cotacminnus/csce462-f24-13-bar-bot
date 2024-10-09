9ddda60dd9d39133c6895eafe9441f5edc99c659
Rokossuwakowski <cotacminnus@gmail.com>
2024-10-08 22:14:54 -0500

    added attributes

--- tts.py
+++ tts.py
@@ -3,10 +3,20 @@ import pyttsx3
 # This is implemented as a class for convenience
 class Text2Speech:
     engine = None
-    volume = 100    # in case we need it
+
+    # in case we need these
+    volume = None
+    rate = None     # speed
+    gender = None   # 0: Male, 1: Female
+    
 
     def init(self):
         self.engine = pyttsx3.init()
+
+        #in case we need these
+        self.volume = self.engine.getProperty('volume')
+        self.rate = self.engine.getProperty('rate')
+        self.gender = self.engine.getProperty('voices')
 
     def text_to_speech(self, text):
         self.engine.say(text)
@@ -16,6 +26,9 @@ class Text2Speech:
         # This will probably be blocking
 
         # return speech   # returns an audio file if we decide to implement otherway later
+
+    def stop(self):
+        self.engine.stop()  # stops the engine
 
     '''
 
