70895e02b23fb450aba1bb782aec8e209a41e54f
Rokossuwakowski <cotacminnus@gmail.com>
2024-10-11 23:12:21 -0500

    Using Whisper

--- speech_recog.py
+++ speech_recog.py
@@ -1,4 +1,5 @@
-from pocketsphinx import LiveSpeech
+import os
+import speech_recognition as sr
 
 '''
 This picks up volume from microphone
@@ -6,11 +7,23 @@ for phrase in LiveSpeech():
     print(phrase)
 '''
 
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Listening...")
+    audio = r.listen(source)
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+try:
+    print(f"Whisper API thinks you said {r.recognize_whisper_api(audio, api_key=OPENAI_API_KEY)}")
+except sr.RequestError as e:
+    print(f"Could not request results from Whisper API; {e}")
+
+'''
 def speech_to_text(speech, online):
     # input an audio file, export a string
     # option to use external api
     return text
-
+'''
 def connect(token):
 
     return False
