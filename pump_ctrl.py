b85cdd08960a7bfc99258d0eee6364a32c27c922
Alex Kearney <143655289+K0rndawg@users.noreply.github.com>
2024-10-16 11:37:44 -0500

    Added pump ctrl file and pump test file

---
+++ pump_ctrl.py
@@ -0,0 +1,31 @@
+import time
+import board
+from adafruit_motorkit import MotorKit
+
+kit = MotorKit(i2c=board.I2C())
+
+
+#TODO Calculates how long pump should run to get 
+def convert_floz_to_time():
+    return
+
+#Dispense liquid from chosen pump for t amount of time
+def actuate_pump(pump, t):
+    if pump == 1:
+        kit.motor1.throttle = 1.0
+        time.sleep(t)
+        kit.motor1.throttle = 0.0
+    elif pump == 2:
+        kit.motor2.throttle = 1.0
+        time.sleep(t)
+        kit.motor2.throttle = 0.0
+    elif pump == 3:
+        kit.motor3.throttle = 1.0
+        time.sleep(t)
+        kit.motor3.throttle = 0.0
+    elif pump == 4:
+        kit.motor4.throttle = 1.0
+        time.sleep(t)
+        kit.motor4.throttle = 0.0
+    else:
+        raise ValueError(f"Invalid pump value: {pump}. Please choose a pump between 1 and 4.")
