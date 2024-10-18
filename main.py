0a4add53e7d1f37dd07aaae179d4cb25e0bd4d49
Rokossuwakowski <cotacminnus@gmail.com>
2024-10-17 22:42:07 -0500

    logic

--- main.py
+++ main.py
@@ -15,5 +15,25 @@ def main():
 
     tts.init()
 
+    #Facial recognition will be polling
+    while True:
+        #while(noface):
+            #try to recognize face every second
+            #if face detected, break
+
+        #greet
+
+        #while not asking for drink
+            #wait for user speech
+                #if getting user speech, respond
+                #if asking for drink, goto drink dispensing
+                #if face not recognized, go back to the beginning
+
+        #dispense drink
+        #go back to 26
+
+        continue
+        #complete the cycle
+
 if __name__ == "__main__":
     main()
