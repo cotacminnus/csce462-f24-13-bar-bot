ca395f6beb72f3549edb0d569b9b5be15db1cc89
Cotacminnus <cotacminnus@gmail.com>
2024-11-08 00:28:47 -0600

    testing motors

--- pump_test.py
+++ pump_test.py
@@ -5,7 +5,7 @@ import board
 from adafruit_motorkit import MotorKit
 
 kit = MotorKit(i2c=board.I2C())
-
+'''
 for i in range(1):
     print("Running motor 1")
     kit.motor1.throttle = 1.0
@@ -34,3 +34,16 @@ for i in range(1):
     kit.motor4.throttle = 0
     print("Stopping motor 4")
     time.sleep(0.5)
+'''
+#run motors for # seconds
+kit.motor1.throttle = 1.0
+kit.motor2.throttle = 1.0
+kit.motor3.throttle = 1.0
+kit.motor4.throttle = 1.0
+
+time.sleep(2)
+
+kit.motor1.throttle = 0
+kit.motor2.throttle = 0
+kit.motor3.throttle = 0
+kit.motor4.throttle = 0
