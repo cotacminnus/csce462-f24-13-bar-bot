2adaf5fb3c727f2ccd0547e9b3d254911f50f867
Sean Lee <99452617+sean3-lee@users.noreply.github.com>
2024-11-26 15:04:40 -0600

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,8 @@ def main():
         my_list = [f'"{item}"' for item in l_list]
         f_list = ", ".join(f'"{item}"' for item in l_list)
         tts.text_to_speech("Available drinks are: ")
-        t.speak_list(l_list)
+        t.speak(drink_list_str)
+        t.start_speaking(drink_list_str)
         drinks = str(drinks)
 
 
