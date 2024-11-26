5515896a616723eb2a026262745718a981cc1e3a
Sean Lee <99452617+sean3-lee@users.noreply.github.com>
2024-11-26 16:42:12 -0600

    Update speech_recog.py

--- speech_recog.py
+++ speech_recog.py
@@ -1,3 +1,52 @@
+import time
+import sounddevice as sd
+import queue
+import json
+from vosk import Model, KaldiRecognizer
+class Speech2Text:
+    def __init__(self):
+        self.model = None
+        self.sample_rate = 16000  # Default
+        self.recognizer = None
+        self.audio_queue = None
+    def audio_callback(self, indata, frames, time, status):
+        if status:
+            print(status)
+        self.audio_queue.put(bytes(indata))
+    def init(self, model_path, srate=16000):
+        self.model = Model(model_path)
+        self.sample_rate = srate
+        self.audio_queue = queue.Queue()
+        self.recognizer = KaldiRecognizer(self.model, self.sample_rate)
+    def listen_until_keyword(self, keywords):
+        """
+        Listens until one of the keywords in the list is detected.
+        :param keywords: List of keywords to detect.
+        :return: The recognized keyword if detected.
+        """
+
+        time.sleep(3)
+        if not isinstance(keywords, list):
+            raise ValueError("Keywords must be provided as a list.")
+        with sd.RawInputStream(samplerate=self.sample_rate, blocksize=8000, dtype="int16",
+                               channels=1, callback=self.audio_callback):
+            print("Listening...")
+            while True:
+                # Get audio data from the queue
+                data = self.audio_queue.get()
+                # Pass data to the recognizer
+                if self.recognizer.AcceptWaveform(data):
+                    result = json.loads(self.recognizer.Result())
+                    print("Recognized:", result["text"])
+                    recognized_text = result["text"].lower()
+                    
+                    # Check if any keyword is found in the recognized text
+                    for keyword in keywords:
+                        if keyword.lower() in recognized_text:
+                            print(f"Keyword detected: {keyword}")
+                            return keyword
+
+'''
 import sounddevice as sd
 import queue
 import json
@@ -42,5 +91,5 @@ class Speech2Text:
                     buf = result["text"]
             print("Hit!")
 
-
+'''
 
