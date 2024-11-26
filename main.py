569b381c9cd0fd326e01546e34cb49bd82dc7789
Sean Lee <99452617+sean3-lee@users.noreply.github.com>
2024-11-26 14:32:23 -0600

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def main():
         drink_list_str = ", ".join(available_drinks)
         l_list = [item.lower() for item in available_drinks]
         my_list = [f'"{item}"' for item in l_list]
-        f_list = ", ".join(f'"{item}"' for item in my_list)
+        f_list = ", ".join(f'"{item}"' for item in l_list)
         print(f_list)
         tts.text_to_speech("Available drinks are: ")
         tts.text_to_speech(f_list)
