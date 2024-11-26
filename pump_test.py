07fa74fc320dfc552f6ad84fbda68b95a7874085
Sean Lee <99452617+sean3-lee@users.noreply.github.com>
2024-11-25 19:42:48 -0600

    Update pump_test.py

--- pump_test.py
+++ pump_test.py
@@ -37,13 +37,13 @@ for i in range(1):
 '''
 #run motors for # seconds
 kit.motor1.throttle = 1.0
-kit.motor2.throttle = 1.0
-kit.motor3.throttle = 1.0
-kit.motor4.throttle = 1.0
+#kit.motor2.throttle = 1.0
+#kit.motor3.throttle = 1.0
+#kit.motor4.throttle = 1.0
 
-time.sleep(6)
+time.sleep(1)
 
 kit.motor1.throttle = 0
-kit.motor2.throttle = 0
-kit.motor3.throttle = 0
-kit.motor4.throttle = 0
+#kit.motor2.throttle = 0
+#kit.motor3.throttle = 0
+#kit.motor4.throttle = 0
