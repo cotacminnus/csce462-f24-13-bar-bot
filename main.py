18b47f8f0ac8e6f19ee9902f114398876423395e
Sean Lee <99452617+sean3-lee@users.noreply.github.com>
2024-11-26 18:13:46 -0600

    Update main.py

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def main():
         if not available_drinks:
             tts1.text_to_speech("I'm sorry, we're out of stock for all drinks.")
             continue
-
+        print(available_drinks)
 
         x = "hi"
         drink_list_str = " ".join(available_drinks)
