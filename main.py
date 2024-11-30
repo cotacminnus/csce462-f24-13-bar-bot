0328390f5a566d74511464a21ae7334b4aff5aa3
Alex Kearney <143655289+K0rndawg@users.noreply.github.com>
2024-11-29 18:19:43 -0600

    Update main.py

--- main.py
+++ main.py
@@ -46,6 +46,7 @@ def main():
         # Greet the customer and list available drinks
         print("Face detected. Greeted the customer.")
         drink_list_str = "Howdy! Welcome to the bar bot! Available drinks are " + ", ".join(available_drinks)
+        print(drink_list_str)
         tts.text_to_speech(drink_list_str)
         
         
