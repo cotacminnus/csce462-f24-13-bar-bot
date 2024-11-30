68ebbb5d0e09d392c4226fa170b150ca61fa7171
Alex Kearney <143655289+K0rndawg@users.noreply.github.com>
2024-11-29 21:47:57 -0600

    Update speech_recog.py: mute flag added

--- speech_recog.py
+++ speech_recog.py
@@ -11,15 +11,27 @@ class Speech2Text:
         self.sample_rate = 16000  # Default
         self.recognizer = None
         self.audio_queue = None
+        
     def audio_callback(self, indata, frames, time, status):
         if status:
             print(status)
-        self.audio_queue.put(bytes(indata))
+        if not self.muted:  
+            self.audio_queue.put(bytes(indata))
+            
     def init(self, model_path, srate=16000):
         self.model = Model(model_path)
         self.sample_rate = srate
         self.audio_queue = queue.Queue()
         self.recognizer = KaldiRecognizer(self.model, self.sample_rate)
+        
+    def mute(self):
+        """Mute the STT system."""
+        self.muted = True
+        
+    def unmute(self):
+        """Unmute the STT system."""
+        self.muted = False
+    
     def listen_until_keyword(self, keywords):
         """
         Listens until one of the keywords in the list is detected.
@@ -46,51 +58,3 @@ class Speech2Text:
                         if keyword.lower() in recognized_text:
                             print(f"Keyword detected: {keyword}")
                             return keyword
-
-'''
-import sounddevice as sd
-import queue
-import json
-from vosk import Model, KaldiRecognizer
-
-class Speech2Text:
-
-    model = None
-    sample_rate = 16000 #default
-    recognizer = None
-    audio_queue = None
-
-    def audio_callback(self, indata, frames, time, status):
-        if status:
-            print(status)
-        self.audio_queue.put(bytes(indata))
-
-    def init(self, model_path, srate=16000):
-        self.model = Model(model_path)
-        self.sample_rate = srate
-        self.audio_queue = queue.Queue()
-        self.recognizer = KaldiRecognizer(self.model, self.sample_rate)
-
-
-    def listen_until_keyword(self, keyword):
-        
-        with sd.RawInputStream(samplerate=self.sample_rate, blocksize=8000, dtype="int16",
-                       channels=1, callback=self.audio_callback):
-            print("Listening...")
-
-            buf = ""
-
-            while buf.find(keyword) == -1:
-                # Get audio data from the queue
-                data = self.audio_queue.get()
-
-                # Pass data to the recognizer and print results
-                if self.recognizer.AcceptWaveform(data):
-                    result = json.loads(self.recognizer.Result())
-                    print("Recognized:", result["text"])
-
-                    buf = result["text"]
-            print("Hit!")
-
-
-'''
