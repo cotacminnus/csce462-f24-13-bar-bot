922054e909e6c5968dfdf4778b9686e93788b3dd
Alex Kearney <143655289+K0rndawg@users.noreply.github.com>
2024-11-29 20:34:01 -0600

    Update tts.py: adjusted threadlocks

--- tts.py
+++ tts.py
@@ -9,13 +9,18 @@ class TextToSpeech:
         self.lock = threading.Lock()  # Add a threading lock
 
     def _speak(self, text):
-        #with self.lock:  # Ensure only one thread speaks at a time
-        self.engine.say(text)
-        self.engine.runAndWait()
+        with self.lock:  # Ensure only one thread speaks at a time
+            self.engine.say(text)
+            self.engine.runAndWait()
 
     def text_to_speech(self, text):
         thread = threading.Thread(target=self._speak, args=(text,))
         thread.start()
+        
+    def stop(self):
+        # Stop the engine
+        with self.lock:
+            self.engine.stop()
 
 
 class Text2Speech:
@@ -37,7 +42,6 @@ class Text2Speech:
 
     def text_to_speech(self, text):
         self.engine.say(text)
-
         self.engine.runAndWait()
 
     
