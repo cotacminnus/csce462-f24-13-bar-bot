4954e29f69e9372f0d81f1463bb1ed4573b4a486
Sean Lee <99452617+sean3-lee@users.noreply.github.com>
2024-12-01 12:26:19 -0600

    Update main.py

--- main.py
+++ main.py
@@ -37,10 +37,11 @@ def main():
         print(available_drinks)
 
         # Greet the customer and list available drinks
+        tts.clear()
+        
         print("Face detected. Greeted the customer.")
         drink_list_str = "Howdy! Welcome to the bar bot! Available drinks are " + ", ".join(available_drinks)
         print(drink_list_str)
-        tts.clear()
         
         #stt.mute()
         try:
